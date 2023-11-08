f6b136111baa60564362af3ee7be4b2056fbd74d
PracaOl <oliwer.pietrzycki@op.pl>
2023-11-08 13:15:41 +0100

    testing

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 //test
+const express = require('express');
+const app = express();
+
+app.get('/', async (req, res) => {
+    res.send("dziala");
+})
+
+app.get('/listAll', async (req, res) => {
+    //res.header("200");
+    res.write("<h1>Test wyswietlania listy rekordow</h1>");
+    res.end();
+})
+
+app.listen(8000);
