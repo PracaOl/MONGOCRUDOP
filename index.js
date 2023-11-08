32501835ab59ae3f8c2796f8e734fdbd9b99f80d
PracaOl <oliwer.pietrzycki@op.pl>
2023-11-08 13:55:15 +0100

    finalstretch

--- index.js
+++ index.js
@@ -11,9 +11,16 @@ app.get('/listAll', async (req, res) => {
     //res.header("200");
     res.write("<h1>Test wyswietlania listy rekordow</h1>");
     const client = await db.connect();
+    res.write("<table>");
     let list = await db.getAllListings(client);
-    list = list.toString();
-    res.write(list);
+    //list = list.toString();
+    list.forEach(element => {
+        res.write("<tr>");
+        res.write("<td>" + element.listing_url +"<td>");
+        res.write("<td>" + element.name + "</td>");
+        res.write("<tr>");
+    })
+    res.write("</table>");
     db.close(client);
     res.end();
 })
